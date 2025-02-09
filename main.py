e3b38fc8f2b264788f5704cae78f1c3de9957abe
vladimir <wladimir.burlay@gmail.com>
2025-02-09 20:09:49 +0100

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+import fastapi
+import uvicorn
+import fastapi_chameleon
+from starlette.staticfiles import StaticFiles
+from views import index
+app = fastapi.FastAPI()
+
+def main():
+    configure()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+def configure():
+    configure_templates()
+    configure_routes()
+
+
+def configure_templates():
+    fastapi_chameleon.global_init('templates')
+
+
+def configure_routes():
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+    app.include_router(index.router)
+
+
+
+if __name__ == "__main__":
+    main()
+else:
+    configure()
