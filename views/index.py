cb9e45d65e16abbe775cbe9a84e33e5ff5b168a9
vladimir <wladimir.burlay@gmail.com>
2025-02-17 14:17:32 +0100

    main.py

--- views/index.py
+++ views/index.py
@@ -10,6 +10,12 @@ def index(welcome: str = 'here you will find general information'):
     return {
         'welcome': welcome
     }
+@router.get("/index.pt")
+@template()
+def index(welcome: str = 'here you will find general information'):
+    return {
+        'welcome': welcome
+    }
 
 
 @router.get("/tools")
@@ -18,7 +24,20 @@ def tools(welcome: str = 'here you will find functionality'):
     return {
         'welcome': welcome,
         'functions': [
-            {'id': 'Defect ?', 'summary': 'Check whether the details are '
+            {'id': 'Defect', 'summary': 'Check whether the details are '
+                                          'defective.'},
+            {'id': 'Localization',
+             'summary': 'Check where selected details are located'}
+        ]
+    }
+
+@router.get("/tools.pt")
+@template()
+def tools(welcome: str = 'here you will find functionality'):
+    return {
+        'welcome': welcome,
+        'functions': [
+            {'id': 'Defect', 'summary': 'Check whether the details are '
                                           'defective.'},
             {'id': 'Localization',
              'summary': 'Check where selected details are located'}
@@ -32,7 +51,12 @@ def description(welcome: str = 'here you will find general description'):
     return {
         'welcome': welcome
     }
-
+@router.get("/description.pt")
+@template()
+def description(welcome: str = 'here you will find general description'):
+    return {
+        'welcome': welcome
+    }
 
 @router.get("/statistic")
 @template()
@@ -41,6 +65,12 @@ def statistic(welcome: str = 'here you will find statistic'):
         'welcome': welcome
     }
 
+@router.get("/statistic.pt")
+@template()
+def statistic(welcome: str = 'here you will find statistic'):
+    return {
+        'welcome': welcome
+    }
 
 @router.get("/contact")
 @template()
@@ -48,3 +78,23 @@ def contact(welcome: str = 'here you will find contact'):
     return {
         'welcome': welcome
     }
+@router.get("/contact.pt")
+@template()
+def contact(welcome: str = 'here you will find contact'):
+    return {
+        'welcome': welcome
+    }
+
+@router.get("/Defect")
+@template()
+def defect(welcome: str = 'here you will find defect'):
+    return {
+        'welcome': welcome
+    }
+@router.get("/Localization")
+@template()
+def localization(welcome: str = 'here you will find localization'):
+    return {
+        'welcome': welcome
+    }
+
